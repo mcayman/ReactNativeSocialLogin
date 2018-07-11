1ac5ec16095f5f61a5d14baa8d153341410b483b
Murat <muratcayman@gmail.com>
2018-07-11 11:50:52 +0300

    Initialize With Google Login

---
+++ App.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  View
+} from 'react-native';
+
+import LogIn from './components/LogIn';
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <LogIn />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
